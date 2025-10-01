e1bdbaf9797fbbed77fef89da895d057eec2b37b
VladStudent <v.a.rishko@student.khai.edu>
2025-10-02 00:10:32 +0300

    ChatBot 2-st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,14 @@
 package ChatBot;
 
 public class ChatBot {
+    public static void main(String[] args) {
+        String botName = "Botik";  // имя бота
+        int birthYear = 2025;       // год создания
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please remind me your name.");
+        String userName = scanner.nextline();
+        System.out.println("What a great name you have," + userName+ "!");
+    }
 }
