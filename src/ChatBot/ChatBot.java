6cc12e16990a39a9088aea1feda6247e3a1e7f0a
VladStudent <v.a.rishko@student.khai.edu>
2025-10-02 19:29:28 +0300

    ChatBot 3st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,14 +1,44 @@
 package ChatBot;
 
+import java.util.Scanner;
+import java.util.WeakHashMap;
+
 public class ChatBot {
     public static void main(String[] args) {
-        String botName = "Botik";  // имя бота
-        int birthYear = 2025;       // год создания
+        String nameBot = "Botik";
+        int YearOld = 2025;
+        Scanner consol = new Scanner(System.in);
 
-        System.out.println("Hello! My name is " + botName + ".");
-        System.out.println("I was created in " + birthYear + ".");
-        System.out.println("Please remind me your name.");
-        String userName = scanner.nextline();
-        System.out.println("What a great name you have," + userName+ "!");
+
+        System.out.println("Hello!, my name is " +nameBot);
+        System.out.println("I was created in " + YearOld);
+        System.out.println("Please, remind me your name.");
+
+        String  Myname = consol.nextLine();
+        System.out.println("What a great name you have " + Myname + "!");
+
+        System.out.println("Let me guess your age. ");
+
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7. ");
+
+
+        int  remainder3 = consol.nextInt();
+        System.out.println(remainder3);
+
+
+        int  remainder5 = consol.nextInt();
+        System.out.println(remainder3);
+
+
+        int  remainder7 = consol.nextInt();
+        System.out.println(remainder3);
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105 ;
+
+        System.out.println("Your age is " + age + " that a good time to start programming!" );
+
+
+       
     }
+
 }
