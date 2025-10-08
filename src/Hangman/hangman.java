37d77350bba5726202f1c7514e9ee1480b1f41d7
VladStudent <v.a.rishko@student.khai.edu>
2025-10-09 00:31:26 +0300

    Practice 5 Stage2

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,21 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+
+        String secret = "python"; // фиксированное слово для этапа 2
+
+        System.out.print("Guess the word: > ");
+        String guess = scanner.nextLine();
+
+        if (guess.equals(secret)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
