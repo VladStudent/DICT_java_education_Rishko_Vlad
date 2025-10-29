ac5fb620f439835f0bd5f840d7c76450fd691f1e
VladStudent <v.a.rishko@student.khai.edu>
2025-10-29 22:57:46 +0200

    Stage 1 TicTacToe

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,12 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Tic Tac Toe initialized!");
+    }
+}
+
+class Game {}
+class Board {}
+class Player {}
+class Cell {}
