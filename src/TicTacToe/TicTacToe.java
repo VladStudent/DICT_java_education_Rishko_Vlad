ba8f72b0b148a436b00ea99523d11e2d57768587
VladStudent <v.a.rishko@student.khai.edu>
2025-10-29 23:00:10 +0200

    Stage 2 TicTacToe

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -6,7 +6,36 @@ public class TicTacToe {
     }
 }
 
-class Game {}
-class Board {}
-class Player {}
-class Cell {}
+class Cell {
+    private char value;
+
+    public Cell() {
+        this.value = ' ';
+    }
+
+    public boolean isEmpty() { return value == ' '; }
+    public char getValue() { return value; }
+    public void setValue(char value) { this.value = value; }
+}
+
+class Board {
+    private final Cell[][] grid = new Cell[3][3];
+
+    public Board() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                grid[i][j] = new Cell();
+    }
+
+    public void print() {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(grid[i][j].getValue() + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+}
