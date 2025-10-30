c6119846437ae8aadf185938d8cf33ed6cf11ed0
VladStudent <v.a.rishko@student.khai.edu>
2025-10-30 10:00:33 +0200

    Stage 2 MatrixProcessing

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -6,45 +6,33 @@ public class MatrixProcessing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Зчитуємо першу матрицю A
-        int n1 = scanner.nextInt();
-        int m1 = scanner.nextInt();
-        int[][] A = new int[n1][m1];
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < m1; j++) {
-                A[i][j] = scanner.nextInt();
+        // Зчитуємо розміри матриці
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        // Зчитуємо матрицю
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = scanner.nextInt();
             }
         }
 
-        // Зчитуємо другу матрицю B
-        int n2 = scanner.nextInt();
-        int m2 = scanner.nextInt();
-        int[][] B = new int[n2][m2];
-        for (int i = 0; i < n2; i++) {
-            for (int j = 0; j < m2; j++) {
-                B[i][j] = scanner.nextInt();
-            }
-        }
+        // Зчитуємо константу
+        int k = scanner.nextInt();
 
-        // Перевірка на сумісність
-        if (n1 != n2 || m1 != m2) {
-            System.out.println("ERROR");
-            return;
-        }
-
-        // Обчислюємо та виводимо суму
-        int[][] C = new int[n1][m1];
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < m1; j++) {
-                C[i][j] = A[i][j] + B[i][j];
+        // Множимо матрицю на константу
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] *= k;
             }
         }
 
         // Виводимо результат
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < m1; j++) {
-                System.out.print(C[i][j]);
-                if (j < m1 - 1) System.out.print(" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < m - 1) System.out.print(" ");
             }
             System.out.println();
         }
