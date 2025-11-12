7110a88e97256598677584e85404bbea6e7a71e1
VladStudent <v.a.rishko@student.khai.edu>
2025-11-12 23:31:33 +0200

    Stage 12 RockPaperScissors

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,4 +1,25 @@
 package RockPaperScissors;
 
+import java.util.Scanner;
+
 public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+
+        String computerChoice = "";
+        switch (userChoice) {
+            case "rock":
+                computerChoice = "paper";
+                break;
+            case "paper":
+                computerChoice = "scissors";
+                break;
+            case "scissors":
+                computerChoice = "rock";
+                break;
+        }
+
+        System.out.println("Sorry, but the computer chose " + computerChoice);
+    }
 }
