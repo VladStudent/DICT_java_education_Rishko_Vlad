d8511938df3f9d0e14ace76a90afac516a2e2e20
VladStudent <v.a.rishko@student.khai.edu>
2025-11-19 23:25:31 +0200

    Stage 2 CurrencyExchange

--- src/CurrencyExchange/CurrencyExchange.java
+++ src/CurrencyExchange/CurrencyExchange.java
@@ -8,6 +8,11 @@ public class CurrencyExchange {
 
         Scanner scanner = new Scanner(System.in);
 
+        stage1(scanner);
+        stage2(scanner);
+    }
+
+    private static void stage1(Scanner scanner) {
         System.out.println("=== Stage 1 ===");
 
         System.out.print("Please, enter the number of mycoins you have: ");
@@ -19,5 +24,22 @@ public class CurrencyExchange {
         double result = coins * rate;
 
         System.out.println("The total amount of dollars: " + result);
+    }
+
+    private static void stage2(Scanner scanner) {
+        System.out.println("\n=== Stage 2 ===");
+
+        System.out.print("Enter how many mycoins you have: ");
+        double coins = scanner.nextDouble();
+
+        double ars = coins * 0.82;
+        double hnl = coins * 0.17;
+        double aud = coins * 1.9622;
+        double mad = coins * 0.208;
+
+        System.out.printf("I will get %.2f ARS\n", ars);
+        System.out.printf("I will get %.2f HNL\n", hnl);
+        System.out.printf("I will get %.2f AUD\n", aud);
+        System.out.printf("I will get %.2f MAD\n", mad);
     }
 }
