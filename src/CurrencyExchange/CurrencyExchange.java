0b2635c6da506ee9f3216e0a02a7a52b69bacb36
VladStudent <v.a.rishko@student.khai.edu>
2025-11-19 23:25:51 +0200

    Stage 3 CurrencyExchange

--- src/CurrencyExchange/CurrencyExchange.java
+++ src/CurrencyExchange/CurrencyExchange.java
@@ -1,45 +1,34 @@
 package CurrencyExchange;
 
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class CurrencyExchange {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
 
-        stage1(scanner);
-        stage2(scanner);
-    }
+        System.out.println("Enter currency code (example: USD, EUR, ILS): ");
+        String code = scanner.next().toLowerCase();
 
-    private static void stage1(Scanner scanner) {
-        System.out.println("=== Stage 1 ===");
+        String link = "http://www.floatrates.com/daily/" + code + ".json";
 
-        System.out.print("Please, enter the number of mycoins you have: ");
-        double coins = scanner.nextDouble();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(link)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.print("Please, enter the exchange rate: ");
-        double rate = scanner.nextDouble();
+        JSONObject obj = new JSONObject(response.body());
 
-        double result = coins * rate;
-
-        System.out.println("The total amount of dollars: " + result);
-    }
-
-    private static void stage2(Scanner scanner) {
-        System.out.println("\n=== Stage 2 ===");
-
-        System.out.print("Enter how many mycoins you have: ");
-        double coins = scanner.nextDouble();
-
-        double ars = coins * 0.82;
-        double hnl = coins * 0.17;
-        double aud = coins * 1.9622;
-        double mad = coins * 0.208;
-
-        System.out.printf("I will get %.2f ARS\n", ars);
-        System.out.printf("I will get %.2f HNL\n", hnl);
-        System.out.printf("I will get %.2f AUD\n", aud);
-        System.out.printf("I will get %.2f MAD\n", mad);
+        if (obj.has("usd")) {
+            System.out.println("USD rate: " + obj.getJSONObject("usd").getDouble("rate"));
+        }
+        if (obj.has("eur")) {
+            System.out.println("EUR rate: " + obj.getJSONObject("eur").getDouble("rate"));
+        }
     }
 }
